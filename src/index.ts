2ce81588632ecc9da3a92ee435704143f108dcea
Luciano Silva <lcnsilvajf@gmail.com>
2025-07-07 18:11:26 -0300

    test

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
+
